5dc66d8904f2d76e5287d8e8968561c288fa9729
George Park <g5park@ucsd.edu>
2017-02-11 17:17:38 -0800

    Nothing was done

--- app/src/main/java/ngc/sleepy/MethodThreeActivity.java
+++ app/src/main/java/ngc/sleepy/MethodThreeActivity.java
@@ -1,8 +1,12 @@
 package ngc.sleepy;
 
+import android.graphics.PixelFormat;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.Button;
+import android.widget.VideoView;
+import android.view.View;
 public class MethodThreeActivity extends AppCompatActivity {
 
     @Override
